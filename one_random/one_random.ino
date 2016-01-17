229d14f6d1dd5a61554d22de34280e89c69788c2
Karlduino <karlduino@gmail.com>
2016-01-17 09:46:26 -0600

    one_random: fade one in as another fades out

--- one_random/one_random.ino
+++ one_random/one_random.ino
@@ -8,38 +8,37 @@
 
 #define PIN 1
 #define N_LED 5
+const int brightness = 50;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LED, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();
-  strip.show();
-  randomSeed(analogRead(A0));
-}
-
-int brightness = 60;
-
-void loop() {
-  int selected = random(N_LED);
-
   for(int i=0; i<N_LED; i++) {
     strip.setPixelColor(i, strip.Color(0, brightness, brightness));
   }
   strip.show();
 
+  randomSeed(analogRead(A0));
+}
+
+int last_selected = random(N_LED); // last one selected
+
+void loop() {  
+  int selected = random(N_LED);
+  while(selected == last_selected) {
+    selected = random(N_LED);
+  }
+    
   for(int i=1; i<=brightness; i++) {
     strip.setPixelColor(selected, strip.Color(i, brightness-i, brightness));
+    strip.setPixelColor(last_selected, strip.Color(brightness-i, i, brightness));
     strip.show();
-    delay(2000/brightness);
+    delay(1000/brightness);
   }
 
-  delay(2000);
+  delay(1000);
 
-  for(int i=brightness-1; i>=0; i--) {
-    strip.setPixelColor(selected, strip.Color(i, brightness-i, brightness));
-    strip.show();
-    delay(2000/brightness);
-  }
-
+  last_selected = selected;
 }
 
