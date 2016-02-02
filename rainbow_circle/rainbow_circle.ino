59a48993f33bc8afb0ab9bfe0f46ed6c36df0274
Karlduino <karlduino@gmail.com>
2016-02-02 16:57:34 -0600

    rainbow_circle: Remove code not being used.

--- rainbow_circle/rainbow_circle.ino
+++ rainbow_circle/rainbow_circle.ino
@@ -20,19 +20,6 @@ void loop() {
   rainbowCycle(20);
 }
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
