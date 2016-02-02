91f0a30403140f9d52505ea93296662836f08ad9
Karlduino <karlduino@gmail.com>
2016-02-02 16:57:10 -0600

    rainbow_circle: Reverse direction of rainbow cycle

--- rainbow_circle/rainbow_circle.ino
+++ rainbow_circle/rainbow_circle.ino
@@ -38,7 +38,7 @@ void rainbowCycle(uint8_t wait) {
 
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      strip.setPixelColor(N_LED-1-i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
     delay(wait);
